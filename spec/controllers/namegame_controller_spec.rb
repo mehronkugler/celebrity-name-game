d8fa77f4dbe99e6f296b89fba4e04dfc14bc5c70
Mehron Kugler <mehron.kugler@gmail.com>
2015-04-26 19:46:53 -0400

    Starting basic test descriptions

--- spec/controllers/namegame_controller_spec.rb
+++ spec/controllers/namegame_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe NamegameController, type: :controller do
-
+  it "should keep track of player score"
+  it "should keep track of how many rounds the player has finished"
+  it "should create new cookies if they don't exist already"
+  it "should set rounds to 1 if cookie doesn't exist"
 end
