1de713484b55c24fe801e79edaec4a6734ca6af4
Mehron Kugler <mehron.kugler@gmail.com>
2015-04-26 19:46:29 -0400

    Add cookies for rounds and score to controller and index

--- app/controllers/namegame_controller.rb
+++ app/controllers/namegame_controller.rb
@@ -1,2 +1,12 @@
 class NamegameController < ApplicationController
+
+  def index
+    # Create new cookies if they don't exist already
+    cookies[:celebng_score] = 0 if (!cookies[:celebng_score])
+    cookies[:celebng_rounds] = 1 if (!cookies[:celebng_rounds])
+    # pass values of cookies to the index page
+    @score = cookies[:celebng_score]
+    @rounds = cookies[:celebng_rounds]
+  end
+
 end
